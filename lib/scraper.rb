a50244f07ffd8d8b386f21b5f75101c70662a1b2
Guy <guymoustapha@gmail.com>
2020-08-18 09:57:35 +0200

    class scrapping work good

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,18 +1,27 @@
 require 'open-uri'
 require 'nokogiri'
 
-def scraper
-  url = 'https://remotive.io/remote-jobs/software-dev'
-  document = open(url)
-  content = document.read
-  parsed_content = Nokogiri::HTML(content)
-  parsed_content.css('.job-list-item').css('.job-details').each do |job|
-    # job_description = []
-    position = job.css('.position').inner_text
-    company = job.css('.company').inner_text
-    details = job.css('.job-tags').inner_text
-    p "job_description: #{position} , from #{company}, job details: #{details}"
-    p '___________________________________________________'
+class Scraping
+  attr_reader :url, :parsed_content
+  def initialize
+    @url = 'https://remotive.io/remote-jobs/software-dev'
+    @parsed_content = nil
+  end
+
+  def scraper
+    document = open(url)
+    content = document.read
+    parsed_content = Nokogiri::HTML(content)
+    parsed_content.css('.job-list-item').css('.job-details').each do |job|
+      # job_description = []
+      position = job.css('.position').inner_text.gsub(/\s+/, '')
+      company = job.css('.company').inner_text.gsub(/\s+/, '  ')
+      details = job.css('.job-tags').inner_text.gsub(/\s+/, ' ')
+      p "job_description: #{position} , from #{company}, job details: #{details}"
+      p '______________________________________________________________________________________________________________'
+    end
   end
 end
-scraper
+
+sc = Scraping.new
+sc.scraper
